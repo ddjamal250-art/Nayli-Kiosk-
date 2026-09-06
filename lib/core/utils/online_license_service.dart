b386b1e3e82ecd91f14c64d2b1eb8767ca664907
ddjamal250-art <ddjamal250-art@users.noreply.github.com>
2026-09-07 00:58:57 +0100

    fix(build): import foundation.dart for debugPrint in online_license_service

--- lib/core/utils/online_license_service.dart
+++ lib/core/utils/online_license_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import '../data/hive_database.dart';
 import '../utils/sound_service.dart';
